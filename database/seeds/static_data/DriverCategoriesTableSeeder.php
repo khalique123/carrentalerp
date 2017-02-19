5ae8bb8ed547ad4f0ebae87968608f5fc4636fc4
khalique <khalique.mohammed@gmail.com>
2017-02-19 11:22:07 +0530

    no message

--- database/seeds/static_data/DriverCategoriesTableSeeder.php
+++ database/seeds/static_data/DriverCategoriesTableSeeder.php
@@ -14,7 +14,7 @@ class DriverCategoriesTableSeeder extends Seeder
         Eloquent::unguard();
 
         $curl_handle=curl_init();
-curl_setopt($curl_handle, CURLOPT_URL, SITE_PATH . '/database/seeds/static_data/driver_categories_seeder.sql');
+curl_setopt($curl_handle, CURLOPT_URL, '/database/seeds/static_data/driver_categories_seeder.sql');
 $xml_file = curl_exec($curl_handle);
 curl_close($curl_handle);
         $path = '/database/seeds/static_data/driver_categories_seeder.sql';
