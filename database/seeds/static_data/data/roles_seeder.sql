99d1895c5a1a331c39ae9488f5a9f6ced8469c6b
khalique <khalique.mohammed@gmail.com>
2017-02-19 12:17:36 +0530

    no message

--- database/seeds/static_data/data/roles_seeder.sql
+++ database/seeds/static_data/data/roles_seeder.sql
@@ -1,5 +1,5 @@
 INSERT INTO roles ( name, is_active, description) VALUES
-('Admin', 1, 'Administrator'),
-('Front Office', 1, 'Front Office Personnel'),
-('Accountant', 1, 'Role used for accounting purposes'),
-('Read Only', 1, 'IT Read Only Role');
+('Admin', TRUE, 'Administrator'),
+('Front Office', TRUE, 'Front Office Personnel'),
+('Accountant', TRUE, 'Role used for accounting purposes'),
+('Read Only', TRUE, 'IT Read Only Role');
