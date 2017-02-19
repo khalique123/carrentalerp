f991341a7151d765c47110fe9c528bf5dbcd1870
khalique <khalique.mohammed@gmail.com>
2017-02-19 11:01:44 +0530

    no message

--- database/seeds/static_data/DriverCategoriesTableSeeder.php
+++ database/seeds/static_data/DriverCategoriesTableSeeder.php
@@ -13,7 +13,7 @@ class DriverCategoriesTableSeeder extends Seeder
     {
         Eloquent::unguard();
 
-        $filename = "/database/seeds/static_data/diver_categories_seeder.sql";
+        $filename = "/database/seeds/static_data/driver_categories_seeder.sql";
 
         $output = shell_exec('cat '.$filename);
         $path = '/database/seeds/static_data/diver_categories_seeder.sql';
