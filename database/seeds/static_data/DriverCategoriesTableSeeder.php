da75ce38769a3ba5afcf0b3faf717c6b53892247
khalique <khalique.mohammed@gmail.com>
2017-02-19 11:20:52 +0530

    no message

--- database/seeds/static_data/DriverCategoriesTableSeeder.php
+++ database/seeds/static_data/DriverCategoriesTableSeeder.php
@@ -13,11 +13,12 @@ class DriverCategoriesTableSeeder extends Seeder
     {
         Eloquent::unguard();
 
-        $filename = "/database/seeds/static_data/driver_categories_seeder.sql";
-
-        $output = shell_exec('cat '.$filename);
+        $curl_handle=curl_init();
+curl_setopt($curl_handle, CURLOPT_URL, SITE_PATH . '/database/seeds/static_data/driver_categories_seeder.sql');
+$xml_file = curl_exec($curl_handle);
+curl_close($curl_handle);
         $path = '/database/seeds/static_data/driver_categories_seeder.sql';
-        DB::unprepared(file_get_contents($path));
+        DB::unprepared($xml_file);
         
         $this->command->info('Driver Categories table seeded!');
     }
