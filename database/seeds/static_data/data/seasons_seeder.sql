acf9d7c358c7905968d45e7c27f9e7126780663e
khalique <khalique.mohammed@gmail.com>
2017-02-24 17:19:18 +0530

    no message

--- database/seeds/static_data/data/seasons_seeder.sql
+++ database/seeds/static_data/data/seasons_seeder.sql
@@ -1,5 +1,5 @@
 INSERT INTO seasons (name, description, start_date, end_date, display_order, is_active) VALUES
-('Default', 'The default season', '2016-01-01', '2016-12-31', 1, 1),
-('High Season', 'High Season', '2016-01-01', '2016-04-30', 2, 1),
-('Low Season', 'Low season', '2016-05-01', '2016-08-31', 3, 1),
-('Mid Season', 'Mid season', '2016-09-01', '2016-12-31', 4, 1);
+('Default', 'The default season', '2016-01-01', '2016-12-31', 1, TRUE),
+('High Season', 'High Season', '2016-01-01', '2016-04-30', 2, TRUE),
+('Low Season', 'Low season', '2016-05-01', '2016-08-31', 3, TRUE),
+('Mid Season', 'Mid season', '2016-09-01', '2016-12-31', 4, TRUE);
--- database/seeds/static_data/data/vehicle_classes_seeder.sql
+++ database/seeds/static_data/data/vehicle_classes_seeder.sql
@@ -1,11 +1,11 @@
 INSERT INTO vehicle_classes (name, description, is_active, display_order) VALUES
-('Economy', 'Economy', 1, 1),
-('FullSize', 'FullSize', 1, 2),
-('Sport Utility', 'Sport Utility', 1, 3),
-('Compact', 'Compact', 1, 4),
-('Mid Size', 'Mid Size', 1, 5),
-('Minivan', 'Minivan', 1, 6),
-('Convertible', 'Convertible', 1, 7),
-('Luxury', 'Luxury', 1, 8),
-('Full Size Van', 'Full Size Van', 1, 9),
-('Pickup', 'Pickup', 1, 10);
+('Economy', 'Economy', TRUE, 1),
+('FullSize', 'FullSize', TRUE, 2),
+('Sport Utility', 'Sport Utility', TRUE, 3),
+('Compact', 'Compact', TRUE, 4),
+('Mid Size', 'Mid Size', TRUE, 5),
+('Minivan', 'Minivan', TRUE, 6),
+('Convertible', 'Convertible', TRUE, 7),
+('Luxury', 'Luxury', TRUE, 8),
+('Full Size Van', 'Full Size Van', TRUE, 9),
+('Pickup', 'Pickup', TRUE, 10);
